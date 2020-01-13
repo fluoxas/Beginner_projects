063c3d201bb59ddbedc73175ccbe9737c895dd21
Christian Ortiz <Fluoxas@tutanota.de>
2020-01-12 19:18:34 -0500

    refactored code with new functions

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,6 +1,6 @@
 # Cli Rock, Paper, Scissors
-import random
 import sys
+import rockPaperScissorsLogic as rps
 
 print('ROCK, PAPER, SCISSORS')
 
@@ -17,7 +17,7 @@ while True:
     while True:
         print("Enter your move: (r)ock (p)aper (s)cissors or (q)uit")
         player_move = input('> ')
-        
+
         if player_move == 'q':
             sys.exit()
         if player_move == 'r' or player_move == 'p' or player_move == 's':
@@ -25,44 +25,22 @@ while True:
         print(f"Option '{player_move}' not acceptable.")
         
     # display player choice
-    if player_move == 'r':
-        print("Rock versus...")
-    elif player_move == 'p':
-        print("Paper Versus...")
-    elif player_move == 's':
-        print("Scissors versus...")
-    
+    rps.player_choice(player_move)
+
     # display computer choice
-    random_number = random.randint(1, 3)
-    if random_number == 1:
-        computer_move = 'r'
-        print("ROCK")
-    elif random_number == 2:
-        computer_move = 'p'
-        print("PAPER")
-    elif random_number == 3:
-        computer_move = 's'
-        print("SCISSORS")
+    computer_move = rps.computer_choice()
     
-    # display and rcord the win/loss/tie
-    if player_move == computer_move:
-        print("It's a tie!\n")
+    # check if player won
+    result = rps.check_win(player_move, computer_move)
+
+    # record result
+    if result == 'W':
+        print("You Win!")
+        wins += 1
+    elif result == 'L':
+        print("You Lose!")
+        losses += 1
+    elif result == 'T':
+        print("Tie!")
         ties += 1
-    elif player_move == 'r' and computer_move == 's':
-        print("You win!\n")
-        wins += 1
-    elif player_move == 'p' and computer_move == 'r':
-        print("You win!\n")
-        wins += 1
-    elif player_move == 's' and  computer_move == 'p':
-        print("You win!\n")
-        wins += 1
-    elif player_move == 'r' and computer_move == 'p':
-        print("You Lose!\n")
-        losses += 1
-    elif player_move == 'p' and computer_move == 's':
-        print("You Lose!\n")
-        losses += 1
-    elif player_move == 's' and computer_move == 'r':
-        print("You Lose!\n")
-        losses += 1
+    print("")
